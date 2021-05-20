9e5dc2b904b25c81137d4f08c0902903b156a21f
Alice Kile <buoyantair@protonmail.com>
2021-05-20 12:21:52 +0530

    Commit

--- src/com/javafun/Main.java
+++ src/com/javafun/Main.java
@@ -2,7 +2,10 @@ package com.javafun;
 
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.print("Hello world");
+        String name = "Alice";
+        System.out.println("Hello World");
+        System.out.println(name);
     }
 }
